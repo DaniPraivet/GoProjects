06e88ec65cac077dd1306e8dc1a95f34b382159f
DaniPraivet <danipraivet@gmail.com>
2025-05-29 21:01:25 +0200

    chore: Infinite loop with an exit option added

--- exercises/CLI Calculator/main.go
+++ exercises/CLI Calculator/main.go
@@ -2,35 +2,43 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	var x, y float64
-	var option string
+	for {
+		var x, y float64
+		var option string
 
-	fmt.Println("Input your first number: ")
-	fmt.Scan(&x)
+		fmt.Println("Input your type of operation (+, -, *, /) or type 'exit' to disconnect: ")
+		fmt.Scan(&option)
 
-	fmt.Println("Input your second number: ")
-	fmt.Scan(&y)
-
-	fmt.Println("Input your type of operation (+, -, *, /): ")
-	fmt.Scan(&option)
-
-	switch option {
-	case "+":
-		fmt.Printf("%.2f %s %.2f = %.2f", x, option, y, x+y)
-	case "-":
-		fmt.Printf("%.2f %s %.2f = %.2f", x, option, y, x-y)
-	case "*":
-		fmt.Printf("%.2f %s %.2f = %.2f", x, option, y, x*y)
-	case "/":
-		if y != 0 {
-			fmt.Printf("%.2f %s %.2f = %.2f", x, option, y, x/y)
-		} else {
-			fmt.Printf("%.2f %s %.2f = %s", x, option, y, "∞")
+		if strings.ToLower(option) == "exit" {
+			fmt.Println("o7")
+			break
 		}
-	default:
-		fmt.Println("Invalid option.")
+
+		fmt.Println("Input your first number: ")
+		fmt.Scan(&x)
+
+		fmt.Println("Input your second number: ")
+		fmt.Scan(&y)
+
+		switch option {
+		case "+":
+			fmt.Printf("%.2f %s %.2f = %.2f \n", x, option, y, x+y)
+		case "-":
+			fmt.Printf("%.2f %s %.2f = %.2f \n", x, option, y, x-y)
+		case "*":
+			fmt.Printf("%.2f %s %.2f = %.2f \n", x, option, y, x*y)
+		case "/":
+			if y != 0 {
+				fmt.Printf("%.2f %s %.2f = %.2f \n", x, option, y, x/y)
+			} else {
+				fmt.Printf("%.2f %s %.2f = %s \n", x, option, y, "∞")
+			}
+		default:
+			fmt.Println("Invalid option.")
+		}
 	}
 }
